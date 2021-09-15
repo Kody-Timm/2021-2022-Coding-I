9cf2d5ba24b9609f5492e716e8496c19b7268741
Kody Timm <timmko@hcosd.org>
2021-09-15 16:11:51 +0000

    added my own File Directory

---
+++ Kody/population.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int startingSize;
+    do
+    {
+        startingSize = get_int("Starting Size: ");
+        
+    }
+    while (startingSize < 9);
+
+    // TODO: Prompt for end size
+    int endingSize;
+    do
+    {
+        endingSize = get_int("Ending Size: ");
+        
+    }
+    while (endingSize < startingSize);
+
+
+    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+    int totalPop = startingSize;
+    do
+    {
+        //calculate total pop
+        totalPop += (totalPop / 3) - (totalPop / 4);
+        years++;
+        
+    }
+    // while total pop > endingSize
+    while ( totalPop < endingSize);
+
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
+}
